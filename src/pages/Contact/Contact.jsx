afad0406b9ac6961622951b673203940f253d7d9
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-19 15:12:06 -0500

    writing code for contact.jsx and home.jsx

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Contact = () => {
+    return (
+        <>
+            <Header />
+            <h1>Contact page</h1>
+            <Footer />
+        </>
+    );
+};
+
+export default Contact;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <h1>Home page</h1>
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
