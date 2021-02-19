8204a3b47fda5eb398a0ed4ac18a34c0c11ee2d5
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-19 15:14:23 -0500

    writing code for portfolio.jsx and resume.jsx

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Portfolio = () => {
+    return (
+        <>
+            <Header />
+            <h1>Portfolio page</h1>
+            <Footer />
+        </>
+    );
+};
+
+export default Portfolio;
---
+++ src/pages/Resume/Resume.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Resume = () => {
+    return (
+        <>
+            <Header />
+            <h1>Resume page</h1>
+            <Footer />
+        </>
+    );
+};
+
+export default Resume;
