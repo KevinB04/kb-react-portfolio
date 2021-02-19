36d39796a468a92bfbdb5a4091f944167216e8ce
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-19 16:21:10 -0500

    adding Bootstrap cdn, importing routes and writing code for app.js, and importing for index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Contact from './pages/Contact/Contact';
-import Home from './pages/Home/Home';
-import Portfolio from './pages/Portfolio/Portfolio';
-import Resume from './pages/Resume/Resume';
+import React, { useState } from 'react';
+// import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  
+function App() {
+  const [error, setError] = useState("");
+  return (
+    <div className="App">
+      <AlertContext.Provider value={{ error, setError }}>
+        <Router basename="">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Portfolio" component={Portfolio} />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/Resume" component={Resume} />
+          </Switch>
+        </Router>
+      </AlertContext.Provider>
+    </div>
+  );
 }
 
 export default App;
