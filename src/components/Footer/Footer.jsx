3cbb12828ec4025d8f3352aaac0e78cc8a318ba4
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-19 15:20:07 -0500

    writing code for footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="wrapper">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Portfolio">Portfolio</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
+                    <li><Link to="/Resume">Resume</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
