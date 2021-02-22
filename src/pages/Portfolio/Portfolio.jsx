235bac17cd3d7fc82cf9374175d476ef62ddc794
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-22 16:41:01 -0500

    Creating portfolio page and adding code with screenshots

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -4,11 +4,32 @@ import Footer from "../../components/Footer/Footer";
 
 const Portfolio = () => {
     return (
-        <>
-            <Header />
-            <h1>Portfolio page</h1>
-            <Footer />
-        </>
+        <div className="container mt-3">
+            <div className="row">
+            </div>
+            <div className="col-lg-12">
+            </div>
+            <h1 className="display-4">Portfolio</h1>
+            <hr className="my-4">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <p><a href="https://evening-headland-02577.herokuapp.com/">Wavelength</a></p>
+                        <img src="./assets/images/Home.jpg" className="m-1"></img>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <p><a href="https://kevinb04.github.io/day-planner/" alt="Day Planner">Day Planner</a></p>
+                        <img src="./assets/images/Daily Planner.jpg" className="m-1"></img>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <p><a href="https://kevinb04.github.io/horiseon-seo/">Horiseon</a></p>
+                    </div>
+                </div>
+            </hr>
+        </div>
     );
 };
 
