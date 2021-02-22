9271f8b2e718fd569d89fd4b7f48178a4ada5f4f
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-22 16:28:59 -0500

    Creating contact info in contact.jsx and adding styling in css page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,15 +1,46 @@
 import React from "react";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import "./Contact.css";
 
 const Contact = () => {
-    return (
-        <>
-            <Header />
-            <h1>Contact page</h1>
-            <Footer />
-        </>
-    );
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-lg-12">
+          <h1 className="display-4">Contact</h1>
+          <hr className="my-4">
+            <div className="row"></div>
+            <p>
+              <b>Email</b> - kbuttimer4@gmail.com
+            </p>
+            <p>
+              <b>Phone</b> - (912)220-9860
+            </p>
+            <div className="row">
+              <div className="col-12">
+                <p1>
+                  <a href="https://github.com/KevinB04">GitHub</a>
+                </p1>
+                <div className="row">
+                  <div className="col-6">
+                    <p2>
+                      <a href="https://www.linkedin.com/in/kevin-buttimer-9b5b3a9b/">
+                        LinkedIn
+                      </a>
+                    </p2>
+                    <p3>
+                      <a href="C:\Users\kbutt\Desktop\Web Development Resume.pdf">
+                        Resume
+                      </a>
+                    </p3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </hr>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
