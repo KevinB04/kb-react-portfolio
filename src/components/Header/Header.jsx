ac671bbb30550663dbd50bca55e5b790518fdeb1
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-22 17:54:37 -0500

    Creating Web Vitals and set up tests files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router, Link} from "react-router-dom";
 
-const Header = () => {
+const Navbar = () => {
     return (
-        <nav>
-            <div className="wrapper">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/Portfolio">Portfolio</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
-                    <li><Link to="/Resume">Resume</Link></li>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="./src/pages/Home/Home">Kevin Buttimer</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+                </button>
+
+            <section className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="./src/pages/Home/Home">About<span className="sr-only">(current)</span></a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a className="nav-link" href=".src/pages/Portfolio/Portfolio">Portfolio</a>        
+                    </li>
+
+                    <li className="nav-item">
+                        <a className="nav-link" href=".src/pages/Contact/Contact">Contact</a>
+                    </li>
                 </ul>
-            </div>
+            </section>
         </nav>
     );
 };
 
-export default Header;
+export default Navbar;
